07a5e2a17df180b96fb985d4ebf5b81258bd1c87
anegussie <aalem@LAPTOP-25FHFF4M.localdomain>
2021-04-04 19:40:06 -0400

    added vpc/subnet

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 
+
+
+git clone https://github.com/anegussie/ec2-elb-sample.git
+cd ec2-elb-sample
+
+make clean
+
+make 
+
+terraform init
+
+terraform plan
+
+terraform apply 
--- sample.tf
+++ sample.tf
@@ -9,10 +9,12 @@ resource "aws_key_pair" "sample-demo" {
 }
 
 resource "aws_instance" "my-instance" {
+        vpc_id = "vpc-01f7626a3c406800e"
+        subnet = "subnet-09bfe0f1c2e40beec" 
 	ami = "ami-0742b4e673072066f"
 	instance_type = "t2.nano"
 	key_name = aws_key_pair.sample-demo.key_name
-	user_data = file("/home/aalem/mygo/install.sh")
+	user_data = file("/home/aalem/mygo/sample")
 	tags = {
 		Name = "Terraform"	
 	}
